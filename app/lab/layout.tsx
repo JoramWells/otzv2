bf757f555c08f7f2c225c893b8c769db04d92138
JoramWells <jorammanoah1@gmail.com>
2024-05-01 10:52:04 +0300

    removed sidebar list  items

--- app/lab/layout.tsx
+++ app/lab/layout.tsx
@@ -5,20 +5,58 @@ import '../globals.css'
 // import { Providers } from '../providers'
 import Navbar from '../_components/Navbar/Nav/Navbar'
 import { Sidebar } from '../_components/dashboard/Sidebar'
-import SidebarItems from '../_components/dashboard/SidebarItems'
-import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import { store } from '@/lib/store'
 import { SidebarProvider } from '@/context/SidebarContext'
+import SidebarListItemsComponent, { type SidebarListItemsProps } from '../_components/patient/SidebarListItemsComponent'
+import { BookCheckIcon, BookCopy, HeartHandshake, InspectionPanel, LayoutDashboardIcon, Users } from 'lucide-react'
+
+const DL: SidebarListItemsProps[] = [
+  {
+    id: '1',
+    label: 'Dashboard',
+    link: '/users/dashboard',
+    icon: <LayoutDashboardIcon size={17} />
+  },
+  {
+    id: '2',
+    label: 'Caregivers',
+    link: '/users/caregiver',
+    icon: <HeartHandshake size={17} />
+  },
+  {
+    id: '3',
+    label: 'Case Managers',
+    link: '/users/casemanager',
+    icon: <InspectionPanel size={17} />
+  },
+  {
+    id: '4',
+    label: 'Patients',
+    link: '/users/patients',
+    icon: <Users size={17} />
+  },
+  {
+    id: '5',
+    label: 'Enrollments',
+    link: 'users/enrollment',
+    icon: <BookCheckIcon size={17} />
+  },
+  {
+    id: '6',
+    label: 'Reports',
+    link: 'viratrack/reports',
+    icon: <BookCopy size={17} />
+  }
+]
 
 const PatientLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
-      <ChakraProvider>
         <SidebarProvider>
           <div className="flex flex-row">
             <Sidebar>
-              <SidebarItems />
+              <SidebarListItemsComponent dataList={DL} />
             </Sidebar>
             <div className="flex flex-col flex-1 h-screen overflow-y-auto">
               <Navbar />
@@ -27,7 +65,6 @@ const PatientLayout = ({ children }: { children: React.ReactNode }) => {
             </div>
           </div>
         </SidebarProvider>
-      </ChakraProvider>
     </Provider>
   )
 }
--- app/treatment-plan/layout.tsx
+++ app/treatment-plan/layout.tsx
@@ -5,29 +5,73 @@ import '../globals.css'
 // import { Providers } from '../providers'
 import Navbar from '../_components/Navbar/Nav/Navbar'
 import { Sidebar } from '../_components/dashboard/Sidebar'
-import SidebarItems from '../_components/dashboard/SidebarItems'
-import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import { store } from '@/lib/store'
 import { SidebarProvider } from '@/context/SidebarContext'
+import SidebarListItemsComponent, { type SidebarListItemsProps } from '../_components/patient/SidebarListItemsComponent'
+import {
+  BookCheckIcon,
+  BookCopy,
+  HeartHandshake,
+  InspectionPanel,
+  LayoutDashboardIcon,
+  Users
+} from 'lucide-react'
+
+const DL: SidebarListItemsProps[] = [
+  {
+    id: '1',
+    label: 'Dashboard',
+    link: '/users/dashboard',
+    icon: <LayoutDashboardIcon size={17} />
+  },
+  {
+    id: '2',
+    label: 'Caregivers',
+    link: '/users/caregiver',
+    icon: <HeartHandshake size={17} />
+  },
+  {
+    id: '3',
+    label: 'Case Managers',
+    link: '/users/casemanager',
+    icon: <InspectionPanel size={17} />
+  },
+  {
+    id: '4',
+    label: 'Patients',
+    link: '/users/patients',
+    icon: <Users size={17} />
+  },
+  {
+    id: '5',
+    label: 'Enrollments',
+    link: 'users/enrollment',
+    icon: <BookCheckIcon size={17} />
+  },
+  {
+    id: '6',
+    label: 'Reports',
+    link: 'viratrack/reports',
+    icon: <BookCopy size={17} />
+  }
+]
 
 const PatientLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
-      <ChakraProvider>
-        <SidebarProvider>
-          <div className="flex flex-row">
-            <Sidebar>
-              <SidebarItems />
-            </Sidebar>
-            <div className="flex flex-col flex-1 h-screen overflow-y-auto">
-              <Navbar />
+      <SidebarProvider>
+        <div className="flex flex-row">
+          <Sidebar>
+            <SidebarListItemsComponent dataList={DL} />
+          </Sidebar>
+          <div className="flex flex-col flex-1 h-screen overflow-y-auto">
+            <Navbar />
 
-              {children}
-            </div>
+            {children}
           </div>
-        </SidebarProvider>
-      </ChakraProvider>
+        </div>
+      </SidebarProvider>
     </Provider>
   )
 }
