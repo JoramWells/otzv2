b5ad0ccd570b6fffaa9fb70c784b09ceee89aa6e
JoramWells <jorammanoah1@gmail.com>
2024-05-31 08:36:35 +0300

    modified sizes

--- app/articles/[bookID]/page.tsx
+++ app/articles/[bookID]/page.tsx
@@ -4,12 +4,35 @@
 import { useDeleteArticlesMutation, useGetAllArticlesQuery } from '@/api/articles/articles.api'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 import { TrashIcon } from 'lucide-react'
+import dynamic from 'next/dynamic'
 import Image from 'next/image'
+
+const BreadcrumbComponent = dynamic(
+  async () => await import('@/components/nav/BreadcrumbComponent'),
+  {
+    ssr: false,
+    loading: () => <Skeleton className="w-full h-[52px] rounded-lg" />
+  }
+)
 
 const loaderProp = ({ src }: { src: string }) => {
   return src
 }
+
+const dataList = [
+  {
+    id: '1',
+    label: 'home',
+    link: '/'
+  },
+  {
+    id: '1',
+    label: 'Dashboard',
+    link: '/administrator/dashboard'
+  }
+]
 
 const ArticlePage = () => {
   const { data } = useGetAllArticlesQuery()
@@ -18,56 +41,60 @@ const ArticlePage = () => {
   console.log(data)
 
   return (
-    <div className="flex flex-row flex-wrap w-full justify-center p-4 gap-4 ">
-      {data?.map((item: any) => (
-        <div key={item.id} className="w-[300px] rounded-xl bg-white relative">
-          <Image
-            // w={0}
-            alt="im"
-            // placeholder="data:image/..."
-            width={300}
-            height={150}
-            // quality={100}
-            // fill
-            // objectFit='contain'
-            // priority
-            layout='fill'
-            className="rounded-t-lg"
-            src={`${process.env.NEXT_PUBLIC_API_URL}/api/articles/${item.image}`}
-            style={{
-              width: '300px',
-              height: '150px',
-              objectFit: 'cover'
-            }}
-            loader={loaderProp}
-          />
+    <>
+      <BreadcrumbComponent dataList={dataList} />
 
-          <div className="p-4">
-            <p className="text-lg font-bold">{item.title}</p>
-
-            <div
-              dangerouslySetInnerHTML={{
-                __html: item.content?.substring(0, 130).concat('..')
+      <div className="grid w-full grid-cols-1 gap-4 lg:grid-cols-5 p-4 md:grid-cols-2">
+        {data?.map((item: any) => (
+          <div key={item.id} className="rounded-xl bg-white">
+            <Image
+              // w={0}
+              alt="im"
+              // placeholder="data:image/..."
+              width={300}
+              height={150}
+              // quality={100}
+              // fill
+              // objectFit='contain'
+              // priority
+              // layout='fill'
+              className="rounded-t-lg"
+              src={`${process.env.NEXT_PUBLIC_API_URL}/api/articles/${item.image}`}
+              style={{
+                width: '300px',
+                height: '150px',
+                objectFit: 'cover'
               }}
+              loader={loaderProp}
             />
-            <Badge className="shadow-none rounded-full bg-slate-200 text-slate-700 hover:bg-slate-200 ">
-              {/* {item.} */}
-              Prep
-            </Badge>
+
+            <div className="p-4">
+              <p className="text-lg font-bold">{item.title}</p>
+
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: item.content?.substring(0, 100).concat('..')
+                }}
+              />
+              <Badge className="shadow-none rounded-full bg-slate-200 text-slate-700 hover:bg-slate-200 ">
+                {/* {item.} */}
+                Prep
+              </Badge>
+            </div>
+            <div className="p-2 w-full bottom-0 right-0">
+              <Button
+                className="bg-slate-200 text-slate-500 hover:bg-slate-100 shadow-none"
+                size={'sm'}
+                onClick={async () => await deleteArticles(item.id)}
+                disabled={isLoading}
+              >
+                <TrashIcon size={18} />
+              </Button>
+            </div>
           </div>
-          <div className='p-2 w-full absolute bottom-0 right-0'>
-            <Button
-              className="bg-slate-200 text-slate-500 hover:bg-slate-100 shadow-none"
-              size={'sm'}
-              onClick={async () => await deleteArticles(item.id)}
-              disabled={isLoading}
-            >
-              <TrashIcon size={18} />
-            </Button>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   )
 }
 
