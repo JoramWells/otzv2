ec5fc50bdecd233ce4b194cff07da6f2ec2f0bc2
JoramWells <jorammanoah1@gmail.com>
2024-10-22 21:32:31 +0300

    added KP indicators

--- app/users/patients/tab/settings/[patientID]/page.tsx
+++ app/users/patients/tab/settings/[patientID]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/non-nullable-type-assertion-style */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
@@ -8,51 +9,53 @@ import { useEffect, useState } from 'react'
 import { useDeletePatientMutation, useGetPatientQuery, useUpdatePatientMutation } from '@/api/patient/patients.api'
 import CustomInput from '@/components/forms/CustomInput'
 import { Button } from '@/components/ui/button'
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
+
 import { Loader2 } from 'lucide-react'
+import BreadcrumbComponent from '@/components/nav/BreadcrumbComponent'
+import CustomSelect from '@/components/forms/CustomSelect'
 
-interface InputProps {
-  value: string
-  setValue: (e: string) => void
-}
+const dataList2 = [
+  {
+    id: '1',
+    label: 'home',
+    link: '/'
+  },
+  {
+    id: '2',
+    label: 'Patients',
+    link: '/'
+  }
+]
 
-const SelectYears = ({ value, setValue }: InputProps) => {
-  // const [value, setValue] = useState()
-  return (
-    <Select
-    onValueChange={e => { setValue(e) }}
-    value={value}
-    >
-      <SelectTrigger className="shadow-none">
-        <SelectValue placeholder="Role" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Years</SelectLabel>
-          <SelectItem value="clinician">Clinician</SelectItem>
-          <SelectItem value="mentor mother">Mentor Mother</SelectItem>
-          <SelectItem value="ayp advocate">AYP Advocate</SelectItem>
-          <SelectItem value="nurse">Nurse</SelectItem>
-          <SelectItem value="patient">Patient</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  )
-}
+const roleData = [
+  {
+    id: 'clinician',
+    label: 'Clinician'
+  },
+  {
+    id: 'mentor mother',
+    label: 'Mentor Mother'
+  },
+  {
+    id: 'ayp advocate',
+    label: 'AYP Advocate'
+  },
+  {
+    id: 'nurse',
+    label: 'Nurse'
+  },
+  {
+    id: 'patient',
+    label: 'Patient'
+  }
+]
 
 const ProfileSettings = ({ params }: { params: any }) => {
   const [phoneNo, setPhoneNo] = useState<string | undefined>('')
   const [firstName, setFirstName] = useState<string | undefined>('')
   const [middleName, setMiddleName] = useState<string | undefined>('')
   const [lastName, setLastName] = useState<string | undefined>('')
+  const [populationType, setPopulationType] = useState<string | undefined>('')
   const [role, setRole] = useState('')
   const { patientID } = params
 
@@ -68,6 +71,7 @@ const ProfileSettings = ({ params }: { params: any }) => {
       setMiddleName(patientProfileData.middleName)
       setLastName(patientProfileData.lastName)
       setRole(patientProfileData.role)
+      setPopulationType(patientProfileData.populationType)
     }
   }, [patientProfileData])
 
@@ -77,15 +81,18 @@ const ProfileSettings = ({ params }: { params: any }) => {
     middleName,
     lastName,
     phoneNo,
-    role
+    role,
+    populationType
   }
 
   console.log(patientProfileData, 'patientProfileData')
 
   return (
     <>
+      <BreadcrumbComponent dataList={dataList2} />
+
       <div className="p-2 ">
-        <div className="w-1/2 flex flex-col space-y-4 bg-white rounded-lg">
+        <div className="w-1/2 p-4 flex flex-col space-y-4 bg-white rounded-lg">
           <CustomInput
             label="First Name"
             value={firstName!}
@@ -106,22 +113,72 @@ const ProfileSettings = ({ params }: { params: any }) => {
           <CustomInput label="Phone" value={phoneNo!} onChange={setPhoneNo} />
 
           {/*  */}
-          <SelectYears setValue={setRole} value={role} />
+          {/* <SelectYears setValue={setRole} value={role} /> */}
+          <CustomSelect
+            label="Role"
+            data={roleData}
+            onChange={setRole}
+            value={role}
+          />
 
-          <div className="flex space-x-4">
+          {/*  */}
+          <CustomSelect
+            label="Key Population"
+            value={populationType as string}
+            onChange={setPopulationType}
+            data={[
+              {
+                id: 'Fisher Folk',
+                label: 'Fisher Folk'
+              },
+              {
+                id: 'FSW',
+                label: 'FSW'
+              },
+              {
+                id: 'General Population',
+                label: 'General Population'
+              },
+              {
+                id: 'MsM',
+                label: 'MsM'
+              },
+              {
+                id: 'PWID',
+                label: 'PWID'
+              },
+
+              {
+                id: 'Truck Drivers',
+                label: 'Truck Drivers'
+              },
+
+              {
+                id: 'TG',
+                label: 'TG'
+              }
+            ]}
+          />
+
+          <div className="flex pt-4 space-x-4 justify-end border-t mt-2">
             <Button
-              disabled={isLoading}
-              onClick={async () => await updatePatient(inputValues)}
+              disabled={isLoadingDeletePatient}
+              onClick={async () => await deletePatient(patientID)}
+              size={'sm'}
+              className="bg-red-50 text-red-500 hover:bg-red-100"
             >
-              {isLoading && <Loader2 className="animate-spin mr-2" size={15} />}
-              Save Changes
+              {isLoadingDeletePatient && (
+                <Loader2 className="animate-spin mr-2" size={15} />
+              )}
+              Delete
             </Button>
             <Button
               disabled={isLoading}
-              onClick={async () => await deletePatient(patientID)}
+              onClick={async () => await updatePatient(inputValues)}
+              size={'sm'}
             >
-              {isLoadingDeletePatient && <Loader2 className="animate-spin mr-2" size={15} />}
-              Delete Account
+              {isLoading && <Loader2 className="animate-spin mr-2" size={15} />}
+              Save
             </Button>
           </div>
         </div>
