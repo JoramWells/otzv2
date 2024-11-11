ac61f1a4ed6fee8d41d6a6315acbfce438c87c72
JoramWells <jorammanoah1@gmail.com>
2024-11-11 15:25:53 +0300

    removed lint error administrator dashboard

--- app/administrator/dashboard/page.tsx
+++ app/administrator/dashboard/page.tsx
@@ -38,12 +38,12 @@ const PieChart = dynamic(
   }
 )
 
-interface DataPops {
-  id: number
-  year: number
-  userGain: number
-  userLost: number
-}
+// interface DataPops {
+//   id: number
+//   year: number
+//   userGain: number
+//   userLost: number
+// }
 
 const dataList = [
   {
@@ -55,39 +55,6 @@ const dataList = [
     id: '1',
     label: 'Dashboard',
     link: '/administrator/dashboard'
-  }
-]
-
-const chartData: DataPops[] = [
-  {
-    id: 1,
-    year: 2016,
-    userGain: 80000,
-    userLost: 823
-  },
-  {
-    id: 2,
-    year: 2017,
-    userGain: 45677,
-    userLost: 345
-  },
-  {
-    id: 3,
-    year: 2018,
-    userGain: 78888,
-    userLost: 555
-  },
-  {
-    id: 4,
-    year: 2019,
-    userGain: 90000,
-    userLost: 4555
-  },
-  {
-    id: 5,
-    year: 2020,
-    userGain: 4300,
-    userLost: 234
   }
 ]
 
