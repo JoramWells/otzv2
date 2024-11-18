f803b081ab02f57c00a7cd424c1af30896c6b87e
JoramWells <jorammanoah1@gmail.com>
2024-11-18 10:05:10 +0300

    modified userData

--- api/patient/data/testData.ts
+++ api/patient/data/testData.ts
@@ -1,5 +1,12 @@
-import { PatientRoles, type PatientAttributes } from 'otz-types'
-
+import { type PatientAttributes } from 'otz-types'
+export declare enum PatientRoles {
+  Admin = 'admin',
+  Clinician = 'clinician',
+  MentorMother = 'mentor mother',
+  AYPAdvocate = 'ayp advocate',
+  Nurse = 'nurse',
+  patient = 'patient',
+}
 export const patientTestData: PatientAttributes = {
   maritalStatus: 'married',
   role: PatientRoles.patient
